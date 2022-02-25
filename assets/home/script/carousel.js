dd8906a4317f65934749000b2bfc952ba13b43f3
aryawidiap <aryawidiaputra2002@gmail.com>
2022-02-25 22:15:58 +0800

    fix new carousel setup

---
+++ assets/home/script/carousel.js
@@ -0,0 +1,17 @@
+//Options
+const options = { loop: false }
+const plugins = [EmblaCarouselAutoplay()] // Plugins
+// Grab wrapper nodes
+const rootNode = document.querySelector('.embla')
+const viewportNode = rootNode.querySelector('.embla__viewport')
+
+// Grab button nodes
+const prevButtonNode = rootNode.querySelector('.embla__prev')
+const nextButtonNode = rootNode.querySelector('.embla__next')
+
+// Initialize the carousel
+const embla = EmblaCarousel(viewportNode, options, plugins)
+
+// Add click listeners
+prevButtonNode.addEventListener('click', embla.scrollPrev, false)
+nextButtonNode.addEventListener('click', embla.scrollNext, false)
