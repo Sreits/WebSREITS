38eace982d3d68de6de05bc8581b3ec3c8a1693b
Abinpramudya <abinpramdya2@gmail.com>
2022-03-06 22:07:06 +0700

    revisi v1
    "tinggal" responsive nya aja

--- assets/merchandise/script/carrousel.js
+++ assets/merchandise/script/carrousel.js
@@ -1,161 +1,40 @@
-const navbar = document.querySelector("#navbar");
+const nextIcon = '<img src ="./assets/merchandise/icon/arrow-right-circle.svg" alt:"right">';
+const prevIcon = '<img src ="./assets/merchandise/icon/arrow-left-circle.svg" alt:"right">';
 
-window.addEventListener("scroll", function () {
-  const currentWindow = window.scrollY;
-
-  if (currentWindow > 0) {
-    navbar.classList.add("navbar-scrolled");
-  }
-  if (currentWindow == 0) {
-    navbar.classList.remove("navbar-scrolled");
-  }
-});
-
-
-
-
-
-
-var myCarousel = document.querySelector('#carouselExampleControls')
-var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 4000,
-  wrap: true
-})
-
-var myCarousel1 = document.querySelector('#carouselExampleSlidesOnly')
-var carousel1 = new bootstrap.Carousel(myCarousel1, {
-  interval: 4000,
-  wrap: true
-})
-
-
-
-var slideIndex = [1,1,1,1];
-/* Class the members of each slideshow group with different CSS classes */
-var slideId = ["mySlides-1", "mySlides-2","mySlides-3","mySlides-4"]
-showSlides(1, 0);
-showSlides(1, 1);
-showSlides(1, 2);
-showSlides(1, 3);
-
-
-
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
-}
-
-function showSlides(n, no) {
-  var i;
-  var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex[no]-1].style.display = "block";
-}
-
-
-const galleryContainer = document.querySelector('.gallery-container');
-const galleryControlsContainer = document.querySelector('.gallery-controls');
-const galleryControls = ['previous', 'next'];
-const galleryItems = document.querySelectorAll('.gallery-item');
-
-class Carousel {
-  constructor(container, items, controls) {
-    this.carouselContainer = container;
-    this.carouselControls = controls;
-    this.carouselArray = [...items];
-  }
-
-  // Update css classes for gallery
-  updateGallery() {
-    this.carouselArray.forEach(el => {
-      el.classList.remove('gallery-item-1');
-      el.classList.remove('gallery-item-2');
-      el.classList.remove('gallery-item-3');
-      el.classList.remove('gallery-item-4');
-      el.classList.remove('gallery-item-5');
-      el.classList.remove('gallery-item-6');
-    });
-
-    this.carouselArray.slice(0, 5).forEach((el, i) => {
-      el.classList.add(`gallery-item-${i+1}`);
-    });
-  }
-
-  // Update the current order of the carouselArray and gallery
-  setCurrentState(direction) {
-
-    if (direction.className == 'gallery-controls-previous') {
-      this.carouselArray.unshift(this.carouselArray.pop());
-    } else {
-      this.carouselArray.push(this.carouselArray.shift());
-    }
-    
-    this.updateGallery();
-  }
-
-  // Construct the carousel navigation
-  // setNav() {
-    // galleryContainer.appendChild(document.createElement('ul')).className = 'gallery-nav';
-
-    // this.carouselArray.forEach(item => {
-    //   const nav = galleryContainer.lastElementChild;
-    //   nav.appendChild(document.createElement('li'));
-    // }); 
-  // }s
-
-  // Construct the carousel controls
-  setControls() {
-    this.carouselControls.forEach(control => {
-      galleryControlsContainer.appendChild(document.createElement('button')).className = `gallery-controls-${control}`;
-
-      document.querySelector(`.gallery-controls-${control}`).innerText = control;
-    });
-  }
- 
-  // Add a click event listener to trigger setCurrentState method to rearrange carousel
-  useControls() {
-    const triggers = [...galleryControlsContainer.childNodes];
-
-    triggers.forEach(control => {
-      control.addEventListener('click', e => {
-        e.preventDefault();
-
-        if (control.className == 'gallery-controls-add') {
-          const newItem = document.createElement('img');
-          const latestItem = this.carouselArray.length;
-          const latestIndex = this.carouselArray.findIndex(item => item.getAttribute('data-index') == this.carouselArray.length)+1;
-
-          // Assign the necessary properties for new gallery item
-          Object.assign(newItem,{
-            className: 'gallery-item',
-            src: `http://fakeimg.pl/300/?text=${this.carouselArray.length+1}`
-          });
-          newItem.setAttribute('data-index', this.carouselArray.length+1);
-
-          // Then add it to the carouselArray and update the gallery
-          this.carouselArray.splice(latestIndex, 0, newItem);
-          document.querySelector(`[data-index="${latestItem}"]`).after(newItem);
-          this.updateGallery();
-
-        } else {
-          this.setCurrentState(control);
+$('.owl-carousel').owlCarousel({
+    center:true,
+    loop:true,
+    margin:-90,
+    nav:true,
+    dots:false,
+    navText: [
+      prevIcon,
+      nextIcon
+    ],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
         }
-
-      });
-    });
-  }
-}
-
-const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
-
-exampleCarousel.setControls();
-// exampleCarousel.setNav();
-exampleCarousel.useControls();
+    }
+})
 
 
-//buat contekan bootstrap
 
+// scroll functions
+$(window).scroll(function(e) {
 
+    // add/remove class to navbar when scrolling to hide/show
+    var scroll = $(window).scrollTop();
+    if (scroll >= 150) {
+        $('.navbar').addClass("navbar-scrolled");
+    } else {
+        $('.navbar').removeClass("navbar-scrolled");
+    }
+
+});
