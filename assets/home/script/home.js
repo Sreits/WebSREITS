db92404e74c57e92d1822122a46f44662a280523
aryawidiap <aryawidiaputra2002@gmail.com>
2022-04-01 20:25:15 +0800

    fix home assets moved navbar config to navbar.js and navbar.css

--- assets/home/script/home.js
+++ assets/home/script/home.js
@@ -1,14 +1,5 @@
 import renderHeaderImg from "./header-img.js";
 import renderSreInsight from "./Sre-insight.js";
 
-document.addEventListener("scroll", ()=>{
-    let nav = document.querySelector('.navbar');
-    if(window.scrollY > nav.style.height) {
-        nav.classList.add('scrolled');
-    } else {
-        nav.classList.remove('scrolled');
-    }
-})
-
 renderHeaderImg();
 renderSreInsight();
