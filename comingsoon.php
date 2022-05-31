cb42c0c54ca175b0cbe0c5a19ad97bd7c8aa6fe7
bagas abiyu putra <66271546+bagasabiyu@users.noreply.github.com>
2022-05-31 07:56:22 +0700

    rename index

---
+++ comingsoon.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Coming Soon</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="assets-comingsoon/images/icons/logo.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets-comingsoon/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link href="assets-comingsoon/fontawesome/all.css" rel="stylesheet">
+	<link href="assets-comingsoon/fontawesome/css/brands.css" rel="stylesheet">
+	<link href="assets-comingsoon/fontawesome/css/solid.css" rel="stylesheet">
+	<link href="assets-comingsoon/fontawesome/css/fontawesome.css" rel="stylesheet">
+	<link href="assets-comingsoon/fontawesome/css/v4-font-face.css" rel="stylesheet" />
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets-comingsoon/vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets-comingsoon/vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets-comingsoon/css/util.css">
+	<link rel="stylesheet" type="text/css" href="assets-comingsoon/css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<!--  -->
+	<div class="simpleslide100">
+		<div class="simpleslide100-item bg-img1" style="background-image: url('assets-comingsoon/images/bg01.jpg');"></div>
+		<div class="simpleslide100-item bg-img1" style="background-image: url('assets-comingsoon/images/bg02.jpg');"></div>
+	</div>
+
+	<div class="flex-col-c-sb size1 overlay1">
+		<!--  -->
+		<div class="w-full flex-w flex-sb-m p-l-80 p-r-80 p-t-22 p-lr-15-sm">
+			<div class="wrappic1 m-r-30 m-t-10 m-b-10">
+				<a href="#"><img src="assets-comingsoon/images/icons/logo putih.png" alt="LOGO"></a>
+			</div>
+
+			
+		</div>
+
+		<!--  -->
+		<div class="flex-col-c-m p-l-15 p-r-15 p-t-50 p-b-120">
+			<h3 class="l1-txt1 txt-center p-b-40 respon1">
+				Something big is coming!
+			</h3>
+
+		</div>
+
+		<!--  -->
+		<div class="flex-w flex-c-m p-b-35">
+			<a href="https://www.tiktok.com/@sre_its" class="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
+				<i class="fa-brands fa-tiktok"></i>
+			</a>
+
+			<a href="https://www.instagram.com/sre_its/" class="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
+				<i class="fa-brands fa-instagram"></i>
+			</a>
+
+			<a href="https://www.linkedin.com/company/sreits" class="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
+				<i class="fa-brands fa-linkedin"></i>
+			</a>
+
+			<a href="https://twitter.com/sre_its" class="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
+				<i class="fa-brands fa-twitter"></i>
+			</a>
+
+			<a href="https://www.youtube.com/channel/UCLypr7EhlB4YlPg5xSZ92nA" class="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
+				<i class="fa-brands fa-youtube"></i>
+			</a>
+		</div>
+	</div>
+
+
+
+	
+
+<!--===============================================================================================-->	
+	<script src="assets-comingsoon/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="assets-comingsoon/vendor/bootstrap/js/popper.js"></script>
+	<script src="assets-comingsoon/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="assets-comingsoon/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="assets-comingsoon/vendor/countdowntime/moment.min.js"></script>
+	<script src="assets-comingsoon/vendor/countdowntime/moment-timezone.min.js"></script>
+	<script src="assets-comingsoon/vendor/countdowntime/moment-timezone-with-data.min.js"></script>
+	<script src="assets-comingsoon/vendor/countdowntime/countdowntime.js"></script>
+	<script>
+		$('.cd100').countdown100({
+			/*Set Endtime here*/
+			/*Endtime must be > current time*/
+			endtimeYear: 0,
+			endtimeMonth: 0,
+			endtimeDate: 35,
+			endtimeHours: 19,
+			endtimeMinutes: 0,
+			endtimeSeconds: 0,
+			timeZone: "" 
+			// ex:  timeZone: "America/New_York"
+			//go to " http://momentjs.com/timezone/ " to get timezone
+		});
+	</script>
+<!--===============================================================================================-->
+	<script src="assets-comingsoon/vendor/tilt/tilt.jquery.min.js"></script>
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+<!--===============================================================================================-->
+	<script src="assets-comingsoon/js/main.js"></script>
+
+</body>
+<script id="urlRewriting">
+var newUrl = "/";
+window.history.pushState('data', 'Title', newUrl);
+</script>
+</html>
--- index.php
+++ index.php
@@ -1,122 +1,371 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<title>Coming Soon</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="assets-comingsoon/images/icons/logo.ico"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="assets-comingsoon/vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link href="assets-comingsoon/fontawesome/all.css" rel="stylesheet">
-	<link href="assets-comingsoon/fontawesome/css/brands.css" rel="stylesheet">
-	<link href="assets-comingsoon/fontawesome/css/solid.css" rel="stylesheet">
-	<link href="assets-comingsoon/fontawesome/css/fontawesome.css" rel="stylesheet">
-	<link href="assets-comingsoon/fontawesome/css/v4-font-face.css" rel="stylesheet" />
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="assets-comingsoon/vendor/animate/animate.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="assets-comingsoon/vendor/select2/select2.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="assets-comingsoon/css/util.css">
-	<link rel="stylesheet" type="text/css" href="assets-comingsoon/css/main.css">
-<!--===============================================================================================-->
-</head>
-<body>
-	
-	<!--  -->
-	<div class="simpleslide100">
-		<div class="simpleslide100-item bg-img1" style="background-image: url('assets-comingsoon/images/bg01.jpg');"></div>
-		<div class="simpleslide100-item bg-img1" style="background-image: url('assets-comingsoon/images/bg02.jpg');"></div>
-	</div>
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home | SRE ITS SC</title>
+    <link
+      rel="icon"
+      type="image/x-icon"
+      href="assets/home/img/SRE_ITS_Logo.png"
+    />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
+      rel="stylesheet"
+    />
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+    <!-- External CSS -->
+    <link rel="stylesheet" href="assets/home/style/home.css" />
+    <link rel="stylesheet" href="kepaladankaki/navbar.css" />
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <script>
+      $(function addNavbar () {
+        $('#header').load('kepaladankaki/header.html')
+      })
+      $(function () {
+        $('#footer').load('kepaladankaki/footer.html');
+      })
+    </script>
+  </head>
+  <body>
+    <script src="assets/home/script/home.js" type="module"></script>
+    <!-- navbar -->
+    <div id="header"></div>
+    <!-- navbar end -->
+    <!-- Jumbotron start -->
+    <section
+      class="d-flex flex-column flex-lg-row-reverse align-items-center min-vh-100"
+      id="hero"
+    >
+      <!--Image caroussel-->
+      <div
+        id="header-img-carousel"
+        class="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-indicators" id="header-carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#header-img-carousel"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#header-img-carousel"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#header-img-carousel"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <!-- carousel inner made with header-img.js -->
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#header-img-carousel"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#header-img-carousel"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+      <!-- End of header img -->
 
-	<div class="flex-col-c-sb size1 overlay1">
-		<!--  -->
-		<div class="w-full flex-w flex-sb-m p-l-80 p-r-80 p-t-22 p-lr-15-sm">
-			<div class="wrappic1 m-r-30 m-t-10 m-b-10">
-				<a href="#"><img src="assets-comingsoon/images/icons/logo putih.png" alt="LOGO"></a>
-			</div>
+      <div class="p-5">
+        <h1 class="">
+          Society of Renewable Energy
+          <u>Institut Teknologi Sepuluh Nopember</u>
+        </h1>
+        <p>Empowering <b>#RenewableEnergy</b> for Indonesia brighter future</p>
+        <div>
+          More info
+          <a href="index.html">
+            <button type="button" class="btn btn-outline-dark more-button p-1">
+              <img
+                src="assets/home/img/arrow.svg"
+                alt=""
+                style="width: 100%;"
+              />
+            </button>
+          </a>
+        </div>
+      </div>
+    </section>
+    <!-- Jumbotron end -->
 
-			
-		</div>
+    <!-- Sponsor Vision Mission section start -->
+    <section
+      id="sponsor-vision-mission"
+      class="container justify-content-evenly justify-items-center w-75 p-5"
+    >
+      <!-- Sponsor -->
+      <section class="sponsor d-flex flex-column justify-content-center">
+        <h2 class="card-title pt-5 pb-2">Our Sponsor</h2>
+        <div class="card border-0">
+          <div class="card-body">
+            <div class="d-flex justify-content-around">
+              <img
+                class="w-25 h-max-160px"
+                src="assets/home/img/Sponsor/PT-Pembangunan-Perumahan.jpg"
+                alt=""
+              />
+              <img
+                class="w-25 h-max-160px"
+                src="assets/home/img/Sponsor/Quarto.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- Vision -->
+      <section class="vision d-flex flex-column justify-content-center">
+        <h2 class="card-title pt-5 pb-2">Our Vision</h2>
+        <div class="card shadow border-0">
+          <div
+            class="card-body ps-5 pe-5 our-text-container d-flex flex-column justify-content-center"
+          >
+            <p class="card-text text-center">
+              Enhance student’s <b>interest and insight</b> into
+              <b>Renewable Energy</b> to be able to apply it.
+            </p>
+          </div>
+        </div>
+      </section>
+      <!-- Mission -->
+      <section class="mission d-flex flex-column justify-content-center">
+        <h2 class="card-title pt-5 pb-2">Our Mission</h2>
+        <div class="card shadow border-0">
+          <div
+            class="card-body ps-5 pe-5 our-text-container d-flex flex-column justify-content-center"
+          >
+            <p class="card-text text-center">
+              Disseminate, accommodate, initiate, and become solution in
+              <b>Renewable Energy</b>.
+            </p>
+          </div>
+        </div>
+      </section>
+    </section>
+    <!---->
+    <section
+      class="d-flex flex-column align-items-center flex-lg-row p-5"
+      id="re-cast"
+    >
+      <section>
+        <h2>Re-Cast</h2>
+        <p class="text-center text-md-start">
+          RE-Cast Podcast with several episodes that aims to educate and
+          entertain all students in Indonesia. The themes raised in RE-Cast
+          start from those related to ITS SRE or campus life
+        </p>
+      </section>
+      <section class="" id="re-cast-articles">
+        <!-- Embla carousel -->
+        <div class="embla">
+          <div class="embla__viewport">
+            <div class="embla__container">
+              <div class="embla__slide">
+                <article class="card border-0" style="width: 24ch;">
+                  <img
+                    class="card-img-top"
+                    src="assets/home/img/Re-Cast/re-cast-article.png"
+                    alt=""
+                  />
+                  <div class="card-body">
+                    <p class="card-text">
+                      Re-Cast 1 :  Mahasiswa Harus Tau SRE!
+                    </p>
+                  </div>
+                </article>
+              </div>
+              <div class="embla__slide">
+                <article class="card border-0" style="width: 24ch;">
+                  <img
+                    class="card-img-top"
+                    src="assets/home/img/Re-Cast/re-cast-article.png"
+                    alt=""
+                  />
+                  <div class="card-body">
+                    <p class="card-text">
+                      Jangan ngaku siap kuliah offline kalau belum denger ini!! - Re-Cast: 2 ft. mas Gandhi Surya
+                    </p>
+                  </div>
+                </article>
+              </div>
+              <div class="embla__slide">
+                <article class="card border-0" style="width: 24ch;">
+                  <img
+                    class="card-img-top"
+                    src="assets/home/img/Re-Cast/re-cast-article.png"
+                    alt=""
+                  />
+                  <div class="card-body">
+                    <p class="card-text">
+                      Re-Cast 3 : Mau Mati Gara-Gara Karantina Di Kostan!
+                    </p>
+                  </div>
+                </article>
+              </div>
+            </div>
+          </div>
+          <button class="embla__prev visually-hidden"><</button>
+          <button class="embla__next visually-hidden">></button>
+        </div>
+        <!-- End of Embla Carousel -->
+      </section>
+    </section>
 
-		<!--  -->
-		<div class="flex-col-c-m p-l-15 p-r-15 p-t-50 p-b-120">
-			<h3 class="l1-txt1 txt-center p-b-40 respon1">
-				Something big is coming!
-			</h3>
+    <!-- Sponsor Vision Mission section end -->
 
-		</div>
+    <!-- Our YouTube-->
+    <section id="youtube" class="p-5 d-flex flex-column align-items-center">
+      <h2>Our YouTube</h2>
+      <section
+        class="d-flex flex-column align-items-center flex-md-row mt-5 justify-content-md-center mr"
+        id="video-articles"
+      >
+        <article class="card border-0 shadow mb-5 youtube-article me-md-5">
+          <img
+            class="card-img-top"
+            src="assets/home/img/YouTube/YouTube_SRE-ITS-Goes-to-Jakarta.png"
+            alt=""
+          />
+          <div class="card-body">
+            <p class="card-text">
+              SRE ITS Goes to Jakarta
+            </p>
+          </div>
+        </article>
+        <article class="card border-0 shadow mb-5 youtube-article ms-lg-5">
+          <img
+            class="card-img-top"
+            src="assets/home/img/YouTube/YouTube_Carreer-Talk-1.png"
+            alt=""
+          />
+          <div class="card-body">
+            <p class="card-text">
+              Career Talk 1
+            </p>
+          </div>
+        </article>
+      </section>
+      <div>
+        See more
+        <a
+          target="_blank"
+          class="linkOut"
+          href="https://www.youtube.com/channel/UCLypr7EhlB4YlPg5xSZ92nA"
+        >
+          <button type="button" class="btn btn-outline-dark more-button p-1">
+            <img src="assets/home/img/arrow.svg" alt="" style="width: 100%;" />
+          </button>
+        </a>
+      </div>
+    </section>
 
-		<!--  -->
-		<div class="flex-w flex-c-m p-b-35">
-			<a href="https://www.tiktok.com/@sre_its" class="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
-				<i class="fa-brands fa-tiktok"></i>
-			</a>
+    <!-- SRE Insight section start -->
+    <section id="insight" class="p-5  d-flex flex-column align-items-center">
+      <h2>SRE Insight</h2>
+      <p class="pt-3 pb-3 ps-md-5 pe-md-5 text-center">
+        Educational media initiated by the Competency Development Department of
+        SRE ITS by publishing short readings about interesting information about
+        climate and energy.
+      </p>
 
-			<a href="https://www.instagram.com/sre_its/" class="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
-				<i class="fa-brands fa-instagram"></i>
-			</a>
-
-			<a href="https://www.linkedin.com/company/sreits" class="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
-				<i class="fa-brands fa-linkedin"></i>
-			</a>
-
-			<a href="https://twitter.com/sre_its" class="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
-				<i class="fa-brands fa-twitter"></i>
-			</a>
-
-			<a href="https://www.youtube.com/channel/UCLypr7EhlB4YlPg5xSZ92nA" class="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
-				<i class="fa-brands fa-youtube"></i>
-			</a>
-		</div>
-	</div>
-
-
-
-	
-
-<!--===============================================================================================-->	
-	<script src="assets-comingsoon/vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-	<script src="assets-comingsoon/vendor/bootstrap/js/popper.js"></script>
-	<script src="assets-comingsoon/vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-	<script src="assets-comingsoon/vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-	<script src="assets-comingsoon/vendor/countdowntime/moment.min.js"></script>
-	<script src="assets-comingsoon/vendor/countdowntime/moment-timezone.min.js"></script>
-	<script src="assets-comingsoon/vendor/countdowntime/moment-timezone-with-data.min.js"></script>
-	<script src="assets-comingsoon/vendor/countdowntime/countdowntime.js"></script>
-	<script>
-		$('.cd100').countdown100({
-			/*Set Endtime here*/
-			/*Endtime must be > current time*/
-			endtimeYear: 0,
-			endtimeMonth: 0,
-			endtimeDate: 35,
-			endtimeHours: 19,
-			endtimeMinutes: 0,
-			endtimeSeconds: 0,
-			timeZone: "" 
-			// ex:  timeZone: "America/New_York"
-			//go to " http://momentjs.com/timezone/ " to get timezone
-		});
-	</script>
-<!--===============================================================================================-->
-	<script src="assets-comingsoon/vendor/tilt/tilt.jquery.min.js"></script>
-	<script >
-		$('.js-tilt').tilt({
-			scale: 1.1
-		})
-	</script>
-<!--===============================================================================================-->
-	<script src="assets-comingsoon/js/main.js"></script>
-
-</body>
-<script id="urlRewriting">
-var newUrl = "/";
-window.history.pushState('data', 'Title', newUrl);
-</script>
+      <section
+        class="d-flex flex-column align-items-center bg-transparent pt-3 pb-3"
+        id="sre-insight-articles"
+      >
+        <!-- Start of carousel -->
+        <div
+          id="sre-insight-carousel"
+          class="carousel slide"
+          data-bs-ride="carousel"
+        >
+          <!-- carousel inner made with Sre-insight.js -->
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#sre-insight-carousel"
+            data-bs-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#sre-insight-carousel"
+            data-bs-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+        <!-- End of carousel -->
+      </section>
+      <div class="more pt-3">
+        <a href="#">See more</a>
+        <button type="button" class="btn btn-outline-dark more-button p-1">
+          <img src="assets/home/img/arrow.svg" alt="" style="width: 100%;" />
+        </button>
+      </div>
+    </section>
+    <!-- SRE Insight section end -->
+    <!-- scroll top button -->
+    <a href="#" class="scrollTop d-flex justify-content-end me-4 mb-4">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        fill="currentColor"
+        class="bi bi-arrow-up-circle text-decoration-none text-dark"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"
+        />
+      </svg>
+    </a>
+    <!-- end scroll top button -->
+    <!-- footer -->
+    <div id="footer"></div>
+    <!-- end footer -->
+    <script src="kepaladankaki/navbar.js"></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
+    <script src="assets/home/script/embla-carousel.umd.js"></script>
+    <script src="assets/home/script/embla-carousel-autoplay.umd.js"></script>
+    <script src="assets/home/script/carousel.js"></script>
+  </body>
 </html>
